96f6a27be3d91acf847d461ffb87961a87faf4a6
Jade <alemanyjade@gmail.com>
2023-04-12 16:54:59 +0100

    Some naming change

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import './Gallery.scss';
-import logementsData from './data/logements.json';
+import { useFetch } from './hooks/useFetch';
 import Logement from './Logement';
 
-
 function Gallery() {
+  const { isLoading, data: accommodationsData, error } = useFetch(
+    process.env.PUBLIC_URL + '/accommodation.json'
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="gallery">
-      {logementsData.map((logement) => (
-        <Logement key={logement.id} data={logement} />
+      {accommodationsData.map((accommodation) => (
+        <Logement key={accommodation.id} data={accommodation} />
       ))}
     </div>
   );
 }
 
 export default Gallery;
+
