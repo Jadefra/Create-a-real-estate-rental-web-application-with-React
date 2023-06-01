6abe5d193657ff976d4f01fcae9b6695b780da96
Jadefra <91191428+Jadefra@users.noreply.github.com>
2023-06-01 19:28:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,15 @@ import Accommodation from './pages/Accommodation/Accommodation'
 function App() {
   return (
     <React.StrictMode>
-      <BrowserRouter /*</React.StrictMode>basename="/Kasa">*/>
+      <BrowserRouter 
         <Header />
         <Routes>
-          {/* This is a route definition. It tells the router to match the URL `/` and render the `<Home />` component */}
           <Route index path="/" element={<Home />} />
-          {/* It tells the router to match the URL `/accommodation/:accommodationId` and render the component `<Accommodation />` */}
           <Route
                 path="/accommodation/:accommodationId"
                 element={<Accommodation />}
           />
-          {/* Tells the router to match the `/a-propos` URL and render the `<APropos />` component */}
           <Route index path="/a-propos" element={<APropos />} />
-          {/* Tells the router to match any URL and return the `<Error404 />` component. */}
           <Route path="*" element={<Error404 />} />
         </Routes>
         <Footer />
